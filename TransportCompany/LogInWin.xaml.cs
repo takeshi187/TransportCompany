2af71273be1e80cbd4b13d66caf6cc677ddec424
Richard <94477473+kori43@users.noreply.github.com>
2025-01-22 23:14:43 +0500

    Добавьте файлы проекта.

---
+++ TransportCompany/LogInWin.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TransportCompany
+{
+    public partial class LogInWin : Window
+    {
+        public LogInWin()
+        {
+            InitializeComponent();
+        }
+
+        private void Btn_Enter_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Btn_Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Btn_SignUp_Click(object sender, RoutedEventArgs e)
+        {
+            SignUpWin signUpWin = new SignUpWin();
+            signUpWin.Show();
+            this.Close();
+        }
+
+        private void Btn_Clear_Click(object sender, RoutedEventArgs e)
+        {
+            TextBox_Login.Clear();
+            PassBox_Password.Clear();
+        }
+    }
+}
