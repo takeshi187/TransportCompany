2a5d8bf59a4d0a55f6236d91ced6c04bcafd0e6e
Richard <94477473+kori43@users.noreply.github.com>
2025-01-24 00:07:01 +0500

    fixed bugs

--- TransportCompany/LogInWin.xaml.cs
+++ TransportCompany/LogInWin.xaml.cs
@@ -1,9 +1,13 @@
-﻿using System.Windows;
+﻿using System.Data.SqlClient;
+using System.Linq.Expressions;
+using System.Windows;
+using TransportCompany.database;
 
 namespace TransportCompany
 {
     public partial class LogInWin : Window
     {
+        Database database = new Database();
         public LogInWin()
         {
             InitializeComponent();
@@ -11,7 +15,70 @@ namespace TransportCompany
 
         private void Btn_Enter_Click(object sender, RoutedEventArgs e)
         {
+            // Заносим данные из полей в переменные
+            string login = TextBox_Login.Text;
+            string userpassword = PassBox_Password.Password;
 
+            // Проверка на заполненные поля
+            if (login == "")
+                MessageBox.Show("Введите логин!");
+            else if (userpassword == "")
+                MessageBox.Show("Введите пароль!");
+            else 
+            {
+                // Если проверка прошла, делаем запрос к бд
+                string query = "SELECT LastName, PostId FROM Employees WHERE Login = @Login AND Password = @Password";
+                
+                // Открываем подключение
+                database.OpenConnection();
+
+                // Создаем команду и выполняем
+                SqlCommand command = new SqlCommand(query, database.GetConnection());
+
+                command.Parameters.AddWithValue("@Login", login);
+                command.Parameters.AddWithValue("@Password", userpassword);
+
+                // Создаем чтение запроса
+                SqlDataReader reader = command.ExecuteReader();
+                if (reader.Read())
+                {
+                    string LastName = reader.GetString(0);
+                    long PostId = reader.GetInt64(1);
+
+                    try
+                    {
+                        switch(PostId)
+                        {
+                            case 1:
+                                // to do
+                                break;
+                            case 2:
+                                // to do
+                                break;
+                            case 3:
+                                // to do
+                                MessageBox.Show($"Добро пожаловать водитель: {LastName}!");
+                                break;
+                            default:
+                                MessageBox.Show("Некорректная должность пользователя!");
+                                break;
+                        }
+                    }
+                    catch (Exception ex) 
+                    {
+                        MessageBox.Show("Ошибка авторизации:" + ex.Message);
+                    }
+                    finally
+                    {
+                        database.CloseConnection();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Такого аккаунта не существует!");
+                    database.CloseConnection();
+                }
+            }
         }
 
         private void Btn_Exit_Click(object sender, RoutedEventArgs e)
