9fc40360db78f1b6829d38461518f7c500ae0ac8
Richard <94477473+kori43@users.noreply.github.com>
2025-01-24 00:19:19 +0500

    fixed bugs

--- TransportCompany/Classes/Routes.cs
+++ TransportCompany/Classes/Routes.cs
@@ -2,7 +2,7 @@
 {
     public class Routes
     {
-        public int Id { get; private set; }
+        public int RouteId { get; private set; }
         public string StartPoint { get; private set; }
         public string EndPoint { get; private set; }
     }
