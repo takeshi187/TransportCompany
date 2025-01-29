2abb5ca4ffb6041ec397145d3ad6630b569636d7
Richard <94477473+kori43@users.noreply.github.com>
2025-01-29 23:02:27 +0500

    add edit button logic

--- TransportCompany/Windows/EditStatusWin.xaml.cs
+++ TransportCompany/Windows/EditStatusWin.xaml.cs
@@ -21,6 +21,8 @@ namespace TransportCompany.Windows
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             LoadComboBox_EmployeeId();
+            LoadComboBox_EmployeeStatus();
+            LoadComboBox_EmployeePosts();
         }
 
         private void LoadComboBox_EmployeeId()
@@ -64,7 +66,7 @@ namespace TransportCompany.Windows
                 }
                 
                 // Загрузка данных в combobox
-                ComboBox_EmployeeId.DataContext = employees;
+                ComboBox_EmployeeId.ItemsSource = employees;
             }
             catch (Exception ex)
             {
@@ -76,14 +78,143 @@ namespace TransportCompany.Windows
             }       
         }
 
+        private void LoadComboBox_EmployeeStatus()
+        {
+            try
+            {
+                // Очищаем содержимое коллекции
+                statuses.Clear();
+
+                // Открываем подключение
+                database.OpenConnection();
+
+                // Запрос к базе
+                string query = "SELECT * FROM Statuses";
+
+                // Создаем команду
+                SqlCommand command = new SqlCommand(query, database.GetConnection());
+
+                // Выполняем команду
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    // Читаем ответ
+                    Statuses status = new Statuses()
+                    {
+                        StatusId = Convert.ToInt64(reader["StatusId"]),
+                        Status = Convert.ToString(reader["Status"])                    
+                    };
+                    // Заносим данные в коллекцию
+                    statuses.Add(status);
+                }
+
+                // Загрузка данных в combobox
+                ComboBox_EmployeeStatus.ItemsSource = statuses;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при загрузке статусов: {ex.Message}");
+            }
+            finally
+            {
+                database.CloseConnection();
+            }
+        }
+
+        private void LoadComboBox_EmployeePosts()
+        {
+            try
+            {
+                // Очищаем содержимое коллекции
+                posts.Clear();
+
+                // Открываем подключение
+                database.OpenConnection();
+
+                // Запрос к базе
+                string query = "SELECT * FROM Posts";
+
+                // Создаем команду
+                SqlCommand command = new SqlCommand(query, database.GetConnection());
+
+                // Выполняем команду
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    // Читаем ответ
+                    Posts post = new Posts()
+                    {
+                        PostId = Convert.ToInt64(reader["PostId"]),
+                        PostName = Convert.ToString(reader["PostName"])
+                    };
+                    // Заносим данные в коллекцию
+                    posts.Add(post);
+                }
+
+                // Загрузка данных в combobox
+                ComboBox_EmployeePost.ItemsSource = posts;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка при загрузке должности: {ex.Message}");
+            }
+            finally
+            {
+                database.CloseConnection();
+            }
+        }
+
         private void Btn_Back_Click(object sender, RoutedEventArgs e)
         {
-
+            AdminWin adminWin = new AdminWin();
+            adminWin.Show();
+            this.Close();
+        }
+        // to do
+        private int GetPost(int post)
+        {
+            return 0;
         }
 
         private void Btn_Edit_Click(object sender, RoutedEventArgs e)
         {
+            int status = 0;
+            int post = 0;
+            //status = GetStatus(status);
+            post = GetPost(post);
+            try
+            {
+                if(post == 0)
+                {
+                    MessageBox.Show("Не удалось обновить статус");
+                }
+                else
+                {
+                    database.OpenConnection();
 
+                    string query = $"UPDATE Employees set StatusId = @status, PostId = @post WHERE EmployeeId = @employeeid";
+
+                    SqlCommand command = new SqlCommand(query, database.GetConnection());
+
+                    command.Parameters.AddWithValue("@status", ComboBox_EmployeeStatus.Text.ToString());
+                    command.Parameters.AddWithValue("@post", ComboBox_EmployeePost.Text.ToString());
+                    command.Parameters.AddWithValue("@employeeid", ComboBox_EmployeeId.Text.ToString());
+
+                    command.ExecuteNonQuery();
+
+                    MessageBox.Show("Статус успешно изменен!");
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Не удалось обновить статус: " + ex.Message);
+            }
+            finally
+            {
+                database.CloseConnection();
+            }
         }
     }
 }
